1d322c43f64d4f7e112f207ae5aa91e0744c985b
Luke Coy <coy.luke@gmail.com>
2015-09-25 01:46:00 -0400

    scratch, with gradle

---
+++ discogs-service/src/main/java/com/lukecoy/discogs/service/DiscogsService.java
@@ -0,0 +1,7 @@
+package com.lukecoy.discogs.service;
+
+/**
+ * Created by lukecoy on 9/25/15.
+ */
+public class DiscogsService {
+}
---
+++ discogs-service/src/main/java/com/lukecoy/discogs/service/DiscogsServiceGuiceModule.java
@@ -0,0 +1,5 @@
+package com.lukecoy.discogs.service;
+
+public class DiscogsServiceGuiceModule {
+
+}
